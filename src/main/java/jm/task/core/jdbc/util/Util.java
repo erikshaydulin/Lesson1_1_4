97f549ec3bb9b550681732381fdacc70ce0e6b4e
MikeStrat <erikshaydulin@gmail.com>
2023-04-25 19:35:04 +0500

    Реализовал конект с БД в Util.java и дописал User.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String HOST = "jdbc:mysql://localhost:3306/mydbtest";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root1234";
+
+    Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Util () {
+        try {
+            Connection connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
